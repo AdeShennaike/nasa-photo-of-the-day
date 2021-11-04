053398edb48c4c33c71022e14df5ac39db63e202
Ade <shennaike2386@hotmail.com>
2021-11-03 22:57:04 -0400

    testing gitignore

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
   const [nasa, setNasa] = useState([])
 
   useEffect(() => {
-    axios.get(`${BASE_URL}&api_key= ${API_KEY}`)
+    axios.get(`${BASE_URL}?api_key=${API_KEY}`)
     .then(res => 
       console.log(res)
       )
@@ -21,6 +21,7 @@ function App() {
         Read through the instructions in the README.md file to build your NASA
         app! Have fun <span role="img" aria-label='go!'>🚀</span>!
       </p>
+      <img src='{prop.data.url}' alt='The Nebula'/>
     </div>
   );
 }
